5e34fd1a0a18d59552bb8c375bff4e025c881a77
jariwezerhu <jari.wezer@student.hu.nl>
2020-02-19 20:01:55 +0100

    Coded "Normal" difficulty

--- Mastermind/Game.py
+++ Mastermind/Game.py
@@ -41,6 +41,7 @@ def reqDifficulty():
 def playerFeedback(code):
     print("The computer has guessed", code)
     print("{}\n{}\n{}".format("For each number in the correct place, type 2", "For each correct number in the wrong place, type 1", "For each wrong number, type 0"))
+    correct = [0, 0]
     while True:
         try:
             feedback = input("Give your response: ")
@@ -49,11 +50,16 @@ def playerFeedback(code):
             for i in feedback:
                 if not int(i) in (0, 1, 2):
                     raise Exception
-            else:
-                return str(feedback)
+            for i in feedback:
+                if int(i) == 2:
+                    correct[1] += 1
+                if int(i) == 1:
+                    correct[0] += 1
+            return correct
         except:
             print("Input four digits. 0, 1, or 2")
             continue
+
 
 
 def comBraindead():     # niet te verliezen. gokt alleen maar
@@ -75,92 +81,103 @@ def comEasy():         # zelf bedachte algoritme. krijgt de juiste ints en gokt 
     potentialCode = ''
     possibleAnswer = []
     while tries > 0:
-        correct = [0, 0]
         if len(potentialCode) == 4 and possibleAnswer == []:
             for i in permutations(potentialCode):  # puts all permutations in lists
                 possibleAnswer.append(concatenate_list_data(i))     # puts lists into strings and adds permutations to possibleAnswers
             generatedGuess = random.choice(possibleAnswer)
             feedback = playerFeedback(generatedGuess)
-            if int(feedback) == 2222:
-                print("You lost!")
+            if feedback == [0, 4]:
+                print("You lose!")
                 break
         elif not possibleAnswer == []:
             generatedGuess = random.choice(possibleAnswer)
             feedback = playerFeedback(generatedGuess)
-            if int(feedback) == 2222:
-                print("You lost!")
+            if feedback == [0, 4]:
+                print("You lose!")
                 break
         else:
             generatedInt = random.choice(possibleInt)       # random int
             possibleInt.remove(generatedInt)
             generatedGuess = int(str(generatedInt)*4)       # that random int repeated four times, example '1111'
             feedback = playerFeedback(generatedGuess)     # request player feedback
-            if feedback == 2222:
-                print("You lost!")
+            if feedback == [0, 4]:
+                print("You lose!")
                 break
-            for i in feedback:
-                if int(i) == 2:
-                    correct[0] += 1
-            for y in range(0, correct[0]):
+            for y in range(0, feedback[0]):
                 potentialCode += str(generatedInt)
         tries -= 1
     print('You win!')
 
 
+def comNormal():        # Knuth
+    tries = 10
+    possibleInt = [1, 2, 3, 4, 5, 6]
+    possibleAnswer = []
+    for i in permutations(possibleInt, 4):  # puts all permutations in lists
+        possibleAnswer.append(concatenate_list_data(i))     # puts lists into strings and adds permutations to possibleAnswers
+    while tries > 0:
+        if tries == 10:
+            feedback = playerFeedback('1122')
+            if feedback == [0, 4]:
+                print("You lose!")
+                break
+            else:
+                for i in possibleAnswer:
+                    potentialFeedback = generateFeedback(int(i), 1122)
+                    if not feedback == potentialFeedback:
+                        possibleAnswer.remove(i)
+        else:
+            randomCode = random.choice(possibleAnswer)
+            feedback = playerFeedback(randomCode)
+            if feedback == [0, 4]:
+                print("You lose!")
+                break
+            else:
+                for i in possibleAnswer:
+                    potentialFeedback = generateFeedback(int(i), randomCode)
+                    if not feedback == potentialFeedback:
+                        possibleAnswer.remove(i)
+        tries -= 1
+    if tries == 0:
+        print("You win!")
+
+
+def comHard():
+    tries = 10
+    possibleInt = [1, 2, 3, 4, 5, 6]
+    possibleAnswer = []
+    for i in permutations(possibleInt, 4):  # puts all permutations in lists
+        possibleAnswer.append(concatenate_list_data(i))     # puts lists into strings and adds permutations to possibleAnswers
+    while tries > 0:
+        print("X")
+        tries -= 1
+    if tries == 0:
+        print("You win!")
+
 def generateCode():
     code = int(str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)))
     return code
 
+
 def generateFeedback(code, guess):
     correct = [0, 0]    # [correct number, correct place]
-    a1, b1, c1, d1 = str(code)[0], str(code)[1], str(code)[2], str(code)[3]
-    a2, b2, c2, d2 = str(guess)[0], str(guess)[1], str(guess)[2], str(guess)[3]
-    if a2 == a1:                        # first it checks for correct locations, changes them to 0 so they aren't reused
-        correct[1] += 1
-        a1 = 0
-    if b2 == b1:
-        correct[1] += 1
-        b1 = 0
-    if c2 == c1:
-        correct[1] += 1
-        c1 = 0
-    if d2 == d1:
-        correct[1] += 1
-        d1 = 0
-    if a2 == b1:                        # next it checks if in other locations, changes them to 0 so they aren't reused
-        correct[0] += 1
-        b1 = 0
-    if a2 == c1:
-        correct[0] += 1
-        c1 = 0
-    if a2 == d1:
-        correct[0] += 1
-        d1 = 0
-    if b2 == a1:
-        correct[0] += 1
-        a1 = 0
-    if b2 == c1:
-        correct[0] += 1
-        c1 = 0
-    if b2 == d1:
-        correct[0] += 1
-        d1 = 0
-    if c2 == a1:
-        correct[0] += 1
-        a1 = 0
-    if c2 == b1:
-        correct[0] += 1
-        b1 = 0
-    if c2 == d1:
-        correct[0] += 1
-        d1 = 0
-    if d2 == a1:
-        correct[0] += 1
-    if d2 == b1:
-        correct[0] += 1
-    if d2 == c1:
-        correct[0] += 1
+    x = []
+    y = []
+    for i in str(code):
+        x.append(i)    # makes list x of ints in code
+    for i in str(guess):
+        y.append(i)    # makes list y of ints in guess
+    for i in range(0, 4):   # checks correct positions
+        if x[i] == y[i]:
+            correct[1] += 1
+            x[i] = 'a'      # change to random string so it isn't reused
+            y[i] = 'b'
+    for i in range(0, 4):   # checks correct numbers in wrong positions
+        if y[i] in x:
+            correct[0] += 1
+            x.remove(y[i])  # remove so it isn't reused, order doesn't matter anymore
     return correct
+
 
 def playerGuess():
     while True:
@@ -187,14 +204,14 @@ def game():
                 while tries > 0:
                     guess = playerGuess()
                     if str(code) == str(guess):
-                        print("You won. The code is", code)
+                        print("You win! The code is", code)
                         break
                     elif code != guess:
                         feedback = generateFeedback(code, guess)
                         print("The computer gave you feedback. You got", feedback[1], "in the right location, and", feedback[0], "in the wrong location")
                         tries -= 1
                     if tries == 0:
-                        print('You lost. The code was', code)
+                        print('You lose. The code was', code)
                         break
             if gamestyle == 2:      # player guesses
                 reqCode()
@@ -207,7 +224,7 @@ def game():
                         comEasy()
                         break
                     if difficulty == 2:
-                        print("Not yet developed, try 0")
+                        comNormal()
                         continue
                     if difficulty == 3:
                         print("Not yet developed, try 0")
